93e54ef8338c7329976d68caa8d41e44e22b623f
Kindness <>
2026-06-28 19:50:41 +0800

    Revert "fix: skip lsplant bridge during clippy"
    This reverts commit cb927ea793e70d59bb4c468230012e4c9229a368.

--- srx_core/build.rs
+++ srx_core/build.rs
@@ -14,11 +14,6 @@ fn main() {
     println!("cargo:rustc-link-lib=log");
     println!("cargo:rustc-link-lib=android");
 
-    if env::var("CARGO_CFG_CLIPPY").is_ok() {
-        println!("cargo:warning=srx_core: LSPlant bridge skipped under clippy");
-        return;
-    }
-
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
     let is_debug_build = env::var("SRX_BUILD_DEBUG").is_ok();
     build_lsplant_bridge(&target_arch, is_debug_build);
